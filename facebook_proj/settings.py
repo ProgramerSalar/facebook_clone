e16c0e67af6634e11e2d44247ad8317a05ebd678
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-27 12:14:46 +0530

    customize django admin-Modern Interface

--- facebook_proj/settings.py
+++ facebook_proj/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # custom django admin
+    'jazzmin',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,6 +45,13 @@ INSTALLED_APPS = [
     'userauths',
     'core',
     'addon',
+
+
+
+    # Third party apps 
+    'crispy_forms',
+    'taggit',
+    'import_export',
 
     
 ]
@@ -128,3 +138,72 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+
+JAZZMIN_SETTINGS = {
+    'site_header': "Facebook Clone",
+    'site_brand': "Connecting people together...",
+    'site_logo': "images/logo.png",
+    'copyright':  "All Right Reserved 2023",
+    "welcome_sign": "Welcome to Facebook Clone, Login Now.",
+    "topmenu_links": [
+       
+    ],
+
+    "order_with_respect_to": [
+        "core",
+        "userauths",
+        "addon",
+    ],
+    
+    "icons": {
+        "admin.LogEntry": "fas fa-file",
+
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+
+        "userauths.User": "fas fa-user",
+        "userauths.Profile":"fas fa-address-card",
+
+        
+    },
+
+    "show_ui_builder" : True
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": True,
+    "brand_small_text": False,
+    "brand_colour": "navbar-indigo",
+    "accent": "accent-olive",
+    "navbar": "navbar-indigo navbar-dark",
+    "no_navbar_border": False,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-indigo",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "cyborg",
+    "dark_mode_theme": "cyborg",
+    "button_classes": {
+        "primary": "btn-primary",
+        "secondary": "btn-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
+
