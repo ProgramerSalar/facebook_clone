fd6b7be8cec73c8463bfaf9e2a4e3e906cb00454
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-27 16:37:51 +0530

    Register Model in django Admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from .models import User, Profile
 # Register your models here.
 
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['username', 'full_name', 'email', 'gender']
 
-admin.site.register(User)
-admin.site.register(Profile)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'user', 'verified']
+    list_editable = ['verified']
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
