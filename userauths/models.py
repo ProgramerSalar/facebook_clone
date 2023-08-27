d79afd836e35742858314a9db4a95b4d36dcc770
ProgramerSalar <manishkumar60708090@gmail.com>
2023-08-27 14:50:02 +0530

    Creating a custom user model

--- userauths/models.py
+++ userauths/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 
 # Create your models here.
+
+
+GENDER = (
+
+    ('male','Male'),
+    ('female', 'Female'),
+
+)
+
+
+class User(AbstractUser):
+    full_name = models.CharField(max_length=200,null=True)
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=200)
+    gender = models.CharField(max_length=100, choices=GENDER)
+
+    otp = models.CharField(max_length=10, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+    def __str__(self):
+        return self.username
+    
+
